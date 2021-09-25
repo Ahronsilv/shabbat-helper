976973e82a4a6b39341fd9b0fd48668f8fb8bd8e
Haim Aharon Zilberman <ahronsilv@gmail.com>
2021-09-25 16:22:26 +0300

    Initial Commit

---
+++ shabbat-helper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shabbat-helper
+//
+//  Created by Haim Aharon Zilberman on 25/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
